311bc95e36fc6827b5ea75c2468566b8aaa4a872
DW Rodrigues <denys.w.r@hotmail.com>
2022-03-22 21:48:17 -0300

    Form

--- src/app/meu-componente/meu-componente.component.ts
+++ src/app/meu-componente/meu-componente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../models/cliente';
 
 @Component({
   selector: 'app-meu-componente',
@@ -9,7 +10,13 @@ export class MeuComponenteComponent implements OnInit {
 
   constructor() { }
 
+  // CRIAR OBJETO DO TIPO CLIENTE
+  model: Cliente = new Cliente();
+
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log(this.model)
+  }
 }
